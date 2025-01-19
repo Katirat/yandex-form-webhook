2a254734a38e3162b79524eefca710d596f38c00
Katirat <ikateriva@gmail.com>
2025-01-19 23:33:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,8 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.json
-    # Ваш код для обработки данных
-    # Например, отправим сообщение в Telegram
-    telegram_token = 'YOUR_BOT_TOKEN'
-    chat_id = 'YOUR_CHAT_ID'
+    telegram_token = '7908641572:AAEfO9aBUbE26xA5BF_XjQQnpSk3-QFjac4'
+    chat_id = '-1002447941825'
     message = f"Форма заполнена! Данные: {data}"
     requests.post(f'https://api.telegram.org/bot{telegram_token}/sendMessage', data={'chat_id': chat_id, 'text': message})
     return 'OK', 200
